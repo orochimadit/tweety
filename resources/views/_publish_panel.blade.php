628539e88832df392601c1e0f020db0b4a4af97f
Aditya Pratama Badra <orochimadit@gmail.com>
2020-05-01 22:05:58 +0700

    tweety layout

---
+++ resources/views/_publish_panel.blade.php
@@ -0,0 +1,17 @@
+<div class="border border-blue-400 rounded-lg px-8 py-6">
+            <form action="" >
+                <textarea name="body" id="" 
+                class="w-full"
+                placeholder="whats up dude"
+                >
+                
+                </textarea>
+                <hr class="my-4">
+                <footer class="flex justify-between">
+                <img src="https://i.pravatar.cc/40" 
+                    class="rounded-full mr-2"
+                alt="">
+                <button type="submit" class="bg-blue-500 rounded-lg shadow py-4 px-2 text-white">Tweet a roo!</button>
+                </footer>
+            </form>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex">
-    <div class="flex-1">
+    <div class="lg:flex lg:justify-between">
+    <div class="lg:w-32">
         @include('_sidebar_links')
     </div>
-    <div class="flex-1">2</div>
-    <div class="flex-1">3</div>
+    <div class="lg:flex-1 lg:mx-10 " style="max-width:700px ">
+        @include('_publish_panel')
+        <div class="border border-gray-300 rounded-lg">
+        @include('_tweet')
+        @include('_tweet')
+        @include('_tweet')
+        @include('_tweet')
+        </div>
+    </div>
+    <div class="lg:w-1/6 bg-blue-100 rounded-lg">
+        @include('_friends_list')
+    </div>
     </div>
 @endsection
