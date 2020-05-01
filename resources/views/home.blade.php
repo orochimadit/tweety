a8f66ce27e78007f0e259598ad4c612c260e7f6a
Aditya Pratama Badra <orochimadit@gmail.com>
2020-05-01 11:49:54 +0700

    sidebar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+    <div class="flex">
+    <div class="flex-1">
+        @include('_sidebar_links')
     </div>
-</div>
+    <div class="flex-1">2</div>
+    <div class="flex-1">3</div>
+    </div>
 @endsection
